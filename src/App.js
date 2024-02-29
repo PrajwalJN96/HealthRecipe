8670346b171509da3dc2de7643bf3536082ca304
Prajwal <prajwalsuma96@gmail.com>
2024-02-29 11:13:32 +0530

    Basic home page created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import HomePage from './Pages/HomePage';
 import SignupPage from './Pages/SignupPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { FiActivity } from "react-icons/fi";
 function App() {
   return (
     <>
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#"><FiActivity /><FiActivity /><FiActivity /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
@@ -17,21 +18,18 @@ function App() {
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Recipe</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Page
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Recipe</a></li>
+            <li><a className="dropdown-item" href="#">Health</a></li>
             <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
+            <li><a className="dropdown-item" href="#">dashboard</a></li>
           </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
       <form className="d-flex" role="search">
@@ -44,7 +42,7 @@ function App() {
 
 
 
-   <SignupPage/>
+   <HomePage/>
     </>
   );
 }

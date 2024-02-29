6e97b0e41410f2068cfd800ffab6ce394875b45d
Prajwal <prajwalsuma96@gmail.com>
2024-02-29 11:32:20 +0530

    Nav link are working

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
 import './App.css';
 import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
+// import RecipePage from './Pages/RecipePage';
 import SignupPage from './Pages/SignupPage';
+import NotFoundPage from './Pages/NotFoundPage';
+import NavBar from './NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FiActivity } from "react-icons/fi";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
 function App() {
   return (
     <>
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"><FiActivity /><FiActivity /><FiActivity /></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Recipe</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Page
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Recipe</a></li>
-            <li><a className="dropdown-item" href="#">Health</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">dashboard</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+<BrowserRouter>
+    <div className="App">
+      <h1>First react app</h1>
+      <NavBar></NavBar>
+      <div id="page-body">
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/recipe" element={<LoginPage />}></Route>
+        {/* <Route path="/recipe" element={<RecipePage/>}></Route> */}
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
+      </Routes>
+      </div>
     </div>
-  </div>
-</nav>
+    </BrowserRouter>
 
-
-
-   <HomePage/>
+   {/* <HomePage/> */}
     </>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <>
     <div>HomePage</div>
-    <p></p>
+    <p>Welcome to our recipe app, where culinary exploration meets healthy living. Discover a world of delicious recipes tailored to your dietary needs and preferences. Easily search for dishes, filter by nutritional requirements, and explore detailed health ratings for each recipe. Empower your journey to better eating with our intuitive visual representations and make every meal a nutritious delight</p>
     </>
   )
 }
